f7f55f6a23e54fbc88630cb36e0d1d3ba6d87a25
lixuegen <lixuegen@owhat.cn>
2019-05-09 10:56:36 +0800

    日志

--- src/main/java/com/example/dockerjar/api.java
+++ src/main/java/com/example/dockerjar/api.java
@@ -1,5 +1,6 @@
 package com.example.dockerjar;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,10 +12,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class api {
     @RequestMapping("test")
     @ResponseBody
     public String test(){
         return "成功了，哈哈哈！恭喜！";
+    }
+
+    @RequestMapping("test1")
+    @ResponseBody
+    public String test1(){
+        log.warn("====================warn========================");
+        log.error("====================error========================");
+        log.info("====================info========================");
+        log.debug("====================debug========================");
+        return "啦啦啦";
     }
 }
