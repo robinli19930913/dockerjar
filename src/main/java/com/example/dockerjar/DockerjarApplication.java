ff8d13e308bfffdc02b8c19f63db25608c1662e7
lixuegen <lixuegen@owhat.cn>
2019-05-09 11:08:33 +0800

    日志

--- src/main/java/com/example/dockerjar/DockerjarApplication.java
+++ src/main/java/com/example/dockerjar/DockerjarApplication.java
@@ -1,13 +1,16 @@
 package com.example.dockerjar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerjarApplication {
-
+	private static Logger logger = LoggerFactory.getLogger(DockerjarApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DockerjarApplication.class, args);
+		logger.error("====启动成功====");
 		System.out.println("===========启动成功===================");
 	}
 
